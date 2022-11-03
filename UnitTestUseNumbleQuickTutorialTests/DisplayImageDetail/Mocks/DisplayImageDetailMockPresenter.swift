6f301c5442ba445106ad030bf2b7a063551ba382
Papon <papon.s@lawyerplusgroup.com>
2022-11-03 17:34:42 +0700

    [Unit Test][Prepare Mock]  Part 4: Add mock presenter DisplayImageDetailMockPresenter method subclass override func (Reviewed by Papon S.)

--- UnitTestUseNumbleQuickTutorialTests/DisplayImageDetail/Mocks/DisplayImageDetailMockPresenter.swift
+++ UnitTestUseNumbleQuickTutorialTests/DisplayImageDetail/Mocks/DisplayImageDetailMockPresenter.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+@testable import UnitTestUseNumbleQuickTutorial
+
+class DisplayImageDetailMockPresenter: DisplayImageDetailPresenter {
+    
+    var isCallProcessShowStartLoadingView : Bool = false
+    var isCallProcessShowStopLoadingView : Bool = false
+    var isCallProcessShowDisplayImageDetailView : Bool = false
+    var isCallProcessShowGeneralErrorView : Bool = false
+    
+    override func presentDisplayImage(response: DisplayImageDetailModels.DetailModels.Response?) {
+        isCallProcessShowDisplayImageDetailView = true
+    }
+    
+    override func presentStartLoading() {
+        isCallProcessShowStartLoadingView = true
+    }
+    
+    override func presentStopLoading() {
+        isCallProcessShowStopLoadingView = true
+    }
+    
+    override func presentError() {
+        isCallProcessShowGeneralErrorView = true
+    }
+}
