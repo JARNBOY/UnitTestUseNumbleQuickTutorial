51092a18bce3e9046e5083b0d8759ae46ed205ed
Papon <papon.s@lawyerplusgroup.com>
2022-11-03 17:41:19 +0700

    [Unit Test][Prepare Mock]  Part 5: Add mock view controller DisplayImageDetailMockViewController method subclass override func (Reviewed by Papon S.)

--- UnitTestUseNumbleQuickTutorialTests/DisplayImageDetail/Mocks/DisplayImageDetailMockViewController.swift
+++ UnitTestUseNumbleQuickTutorialTests/DisplayImageDetail/Mocks/DisplayImageDetailMockViewController.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+@testable import UnitTestUseNumbleQuickTutorial
+
+class DisplayImageDetailMockViewController: DisplayImageDetailView {
+    
+    var isPresentShowDisplayImage: Bool = false
+    var isPresentShowStartLoadingView: Bool = false
+    var isPresentShowStopLoadingView: Bool = false
+    var isPresentShowError: Bool = false
+    
+    // MARK: DisplayImageDetailView
+    func displayDisplayImage(viewModel: DisplayImageDetailModels.DetailModels.ViewModel) {
+        isPresentShowDisplayImage = true
+    }
+    
+    func displayStartLoading() {
+        isPresentShowStartLoadingView = true
+    }
+    
+    func displayStopLoading() {
+        isPresentShowStopLoadingView = true
+    }
+    
+    func displayError() {
+        isPresentShowError = true
+    }
+    
+}
