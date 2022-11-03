6b2829fdfed148415d89056ca6b0ef8c8c2ddb0f
Papon <papon.s@lawyerplusgroup.com>
2022-11-03 17:29:19 +0700

    [Unit Test][Prepare Mock]  Part 3: Add mock worker DisplayImageDetailMockWorker method subclass override func (Reviewed by Papon S.)

--- UnitTestUseNumbleQuickTutorialTests/DisplayImageDetail/Mocks/DisplayImageDetailMockWorker.swift
+++ UnitTestUseNumbleQuickTutorialTests/DisplayImageDetail/Mocks/DisplayImageDetailMockWorker.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+@testable import UnitTestUseNumbleQuickTutorial
+
+class DisplayImageDetailMockWorker: DisplayImageDetailWorker {
+    var isFail:Bool = false
+    let mock = DisplayImageDetailMock()
+    
+    override func getLoanCancelReasonList(imageRandomId: String, aCompletion: @escaping (DisplayImageDetailModels.DetailModels.Response?) -> Void, fail: @escaping (String?) -> ()) {
+        
+        if !isFail {
+            aCompletion(mock.toPicsumPhotosInfoSuccess())
+        } else {
+            fail(mock.toFailureError())
+        }
+    }
+}
